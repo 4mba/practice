a970e6ed2e59b18df9a4eb6816f6f3ac2498e1c9
Jou Sung-Shik <liks79@gmail.com>
2013-05-21 08:11:47 +0900

    Initial Test Code

--- exif-geotag/exif_reader.py
+++ exif-geotag/exif_reader.py
@@ -1,11 +1,20 @@
-
-
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
 
 import glob
 
+#pyexiv2 를 설치해야 
+#import pyexiv2
 
 
-File_List = glob.glob('./photo/*.jpg')
+
+# exif 정보를 읽어올 사진 파일목록을 가져온다.
+photos = [];
+photos = glob.glob('./photos/*.jpg');
+
+# 파일목록 출
+print photos
+
 
 
 
